3616ede0a314376f857e852354960fa3ddf540f6
Faith Makinde <faithmakinde0@gmail.com>
2022-06-23 20:43:39 +0100

    Task completed

---
+++ life.js
@@ -0,0 +1,8 @@
+let myName = "Faith Makinde";
+let myHeight = "5.5ft";
+let myCountry = "Nigeria";
+alert(`My name is ${myName}`);
+alert(`I am ${myHeight} tall`);
+alert(`I am from ${myCountry}`);
+alert("Thank you for your time 😊💕😍")
+
